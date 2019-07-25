97abcc868f4b7511fb79930c95c05c4f17e1cdb7
조성일 <chosi30629@gmail.com>
2019-07-25 21:32:16 +0900

    Controller, View 구현

---
+++ src/main/java/jpabook/jpashop/web/ItemController.java
@@ -0,0 +1,53 @@
+package jpabook.jpashop.web;
+
+import jpabook.jpashop.domain.item.Book;
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+public class ItemController {
+
+    @Autowired
+    ItemService itemService;
+
+    @GetMapping("/items")
+    public String list(Model model) {
+        List<Item> items = itemService.findItems();
+        model.addAttribute("items", items);
+        return "items/itemList";
+    }
+
+    @GetMapping("/items/new")
+    public String createForm() {
+        return "items/createItemForm";
+    }
+
+    @PostMapping("/items/new")
+    public String create(Book item) {
+        itemService.saveItem(item);
+        return "redirect:/items";
+    }
+
+    @GetMapping("/items/{itemId}/edit")
+    public String updateItemForm(@PathVariable("itemId") Long itemId, Model model) {
+        Item item = itemService.findOne(itemId);
+        model.addAttribute("item", item);
+        return "items/updateItemForm";
+    }
+
+    @PostMapping("/items/{itemId}/edit")
+    public String updateItem(@ModelAttribute("item") Book item) {
+        itemService.saveItem(item);
+        return "redirect:/items";
+    }
+
+}
---
+++ src/main/java/jpabook/jpashop/web/OrderController.java
@@ -0,0 +1,49 @@
+package jpabook.jpashop.web;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.exception.NotEnoughStockException;
+import jpabook.jpashop.service.ItemService;
+import jpabook.jpashop.service.MemberService;
+import jpabook.jpashop.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+public class OrderController {
+
+    @Autowired
+    OrderService orderService;
+
+    @Autowired
+    MemberService memberService;
+
+    @Autowired
+    ItemService itemService;
+
+    @GetMapping("/order")
+    public String createForm(Model model) {
+        List<Member> members = memberService.findMembers();
+        List<Item> items = itemService.findItems();
+
+        model.addAttribute("members", members);
+        model.addAttribute("items", items);
+
+        return "order/orderForm";
+    }
+
+    @PostMapping("/order")
+    public String order(@RequestParam("memberId") Long memberId,
+                        @RequestParam("itemId") Long itemId,
+                        @RequestParam("count") int count) throws NotEnoughStockException {
+        orderService.order(memberId, itemId, count);
+        return "redirect:/orders";
+    }
+
+}
